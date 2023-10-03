c58d7519c8cf8aa2c643129c0cdeb1b440309b02
mikaio_faria <mfaria@gobetterfly.com>
2023-10-03 17:57:24 -0400

    feat:static

--- kitanda_shop/settings.py
+++ kitanda_shop/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import os
 import dj_database_url
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,7 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "whitenoise.runserver_nostatic",
+    "cloudinary",
     "core",
 ]
 
@@ -142,22 +147,18 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howtostatic-files/
-
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
+cloudinary.config(
+    cloud_name="da6sbogam",
+    api_key="446671287474149",
+    api_secret="XEyl9kCf8t8UUz1rfs3pWVChvEU",
+)
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/static/"
--- kitanda_shop/urls.py
+++ kitanda_shop/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     ),
     path("dashboard/", admin.site.urls),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
